cc779d68bd842eb7d4d88ee0e957b5741c09ee4c
35889850 <eran@lig-it.com>
2020-05-29 00:33:54 +0200

    removed unneccesary data

--- wm-zadatak-uporedi-paketa/src/app/app.component.ts
+++ wm-zadatak-uporedi-paketa/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wm-zadatak-uporedi-paketa';
+  title = 'WM Zadatak Uporedi Paketa';
 }
