505fa1bc942ecd69016b7bfd87b4388df1c1a73d
35889850 <eran@lig-it.com>
2020-05-29 15:26:24 +0200

    get correct prices according to contract length

--- wm-zadatak-uporedi-paketa/src/app/card-container/price-section-container/price-section-container.component.ts
+++ wm-zadatak-uporedi-paketa/src/app/card-container/price-section-container/price-section-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-price-section-container',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceSectionContainerComponent implements OnInit {
 
+  @Input() prices: any = null;
+  @Input() selectedContractLength: string;
+
+  get filteredPrices() {
+    console.log('this.prices.old_price_recurring:',
+      this.prices.old_price_recurring,
+      'this.prices.price_recurring:',
+      this.prices.price_recurring,
+      'this.prices.old_price_promo_text:',
+      this.prices.old_price_promo_text
+    );
+
+    return {
+      oldPriceRecurring: Object.keys(this.prices.old_price_recurring).
+        filter(contractLength => contractLength === this.selectedContractLength).
+        map(contractLength => {
+          const index = this.prices.old_price_recurring[contractLength].indexOf('.');
+          const price = this.prices.old_price_recurring[contractLength].slice(0, index);
+          return price;
+        }),
+      priceRecurring: Object.keys(this.prices.price_recurring).
+        filter(contractLength => contractLength === this.selectedContractLength).
+        map(contractLength => {
+          const index = this.prices.price_recurring[contractLength].indexOf('.');
+          const price = this.prices.price_recurring[contractLength].slice(0, index);
+          return price;
+        }),
+      oldPricePromoText: this.cleanPromoText(this.prices.old_price_promo_text)
+    };
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cleanPromoText(text: string) {
+    let cleanText = text.slice(0, text.length - 4);
+    cleanText = cleanText.slice(3, cleanText.length);
+    return cleanText;
   }
 
 }
