238cc7403d7f87f8f48fb2b8f134488078dabc2f
35889850 <eran@lig-it.com>
2020-05-29 04:46:04 +0200

    filter section details according to product category

--- wm-zadatak-uporedi-paketa/src/app/card-container/details-section-container/details-section-container.component.ts
+++ wm-zadatak-uporedi-paketa/src/app/card-container/details-section-container/details-section-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-details-section-container',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-section-container.component.scss']
 })
 export class DetailsSectionContainerComponent implements OnInit {
+
+  @Input() included: any = null;
+  @Input() productCategory: string = null;
 
   constructor() { }
 
